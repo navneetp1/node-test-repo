6b02effffa6f22bc8a7e39bba549ff1f7fa13103
Navneet P <navneet11vinod11@gmail.com>
2023-09-10 20:42:39 +0530

    adding from terminal

---
+++ app.js
@@ -0,0 +1,16 @@
+const os = require('os');
+
+const user = os.userInfo();
+console.log(user);
+
+console.log(`This system has been up for ${Math.floor(os.uptime())} seconds`);
+
+const currOS = {
+  name : os.type(),
+  release : os.release(),
+  totalMemory : os.totalmem(),
+  freeMemory : os.freemem()
+}
+
+console.log(currOS);
+console.log('hello test')
